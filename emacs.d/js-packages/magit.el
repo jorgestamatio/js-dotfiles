cb3fcf940be2681dc7f76406a60652b9eed0cc0a
Jorge Stamatio <jorge.stamatio@gmail.com>
2013-04-29 05:10:30 +0200

    Added js-packages based on rmm5t and submodules

---
+++ emacs.d/js-packages/magit.el
@@ -0,0 +1,56 @@
+;;; It's Magit! An Emacs mode for Git.
+
+(add-hook
+ 'magit-mode-hook
+ (lambda ()
+   (setq yas-dont-activate t)))
+
+(add-hook
+ 'magit-log-edit-mode-hook
+ (lambda ()
+   (setq fill-column 72)
+   (turn-on-auto-fill)))
+
+;; full screen magit-status
+;; borrowed from http://whattheemacsd.com/setup-magit.el-01.html
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+;; Allow ignoring of whitespace
+;; borrowed from http://whattheemacsd.com/setup-magit.el-02.html
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(eval-after-load 'magit
+  '(progn
+     (set-face-background 'highlight nil) ;; highlight is overriding other background colors for diff chunks
+     (set-face-foreground 'highlight nil) ;; highlight is overriding other foreground colors for diff chunks
+     (set-face-underline  'highlight nil)
+     (define-key magit-mode-map (kbd "M-3") 'split-window-horizontally) ; was magit-show-level-3
+     (define-key magit-mode-map (kbd "M-2") 'split-window-vertically)   ; was magit-show-level-2
+     (define-key magit-mode-map (kbd "M-1") 'delete-other-windows)      ; was magit-show-level-1
+     (define-key magit-mode-map (kbd "<tab>") 'magit-toggle-section)    ; was smart-tab
+     (define-key magit-status-mode-map (kbd "M-K") 'magit-quit-session)
+     (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+     ))
